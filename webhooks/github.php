c50e7dfadd89708d7f213cabaabbbc2782717b86
Germanunkol <germanunkol@yahoo.com>
2014-05-24 20:42:38 +0200

    Updated update script.

--- webhooks/github.php
+++ webhooks/github.php
@@ -4,12 +4,13 @@
 	<title>Github update script</title>
 </head>
 <body>
-Updated
+Updating...
 	<?php
 		//chdir(dirname(__FILE__));
-		$result = shell_exec("git pull");
+		$result = shell_exec("/usr/bin/git pull 2>&1");
 		//$result = shell_exec("whoami");
 		echo "<pre>$result</pre>" ;
+		//echo shell_exec("whoami");
 	?>
 </body>
 </html>
