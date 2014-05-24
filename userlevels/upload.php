31fb50a8a02fa1de8d8fe3da7fbc3f3050fd75ae
Germanunkol <germanunkol@yahoo.com>
2014-05-24 18:42:13 +0200

    Added file upload via post command.

---
+++ userlevels/upload.php
@@ -0,0 +1,29 @@
+<?php
+	$DATA = $HTTP_RAW_POST_DATA;
+
+	if(preg_match( "/Name: ([^\n]+)/", $DATA, $results ))
+	{
+		$levelname = $results[1];
+	}
+
+	if(preg_match( "/Creator: ([^\n]+)/", $DATA, $results ))
+	{
+		$creator = $results[1];
+	}
+
+	$pos = strpos( $DATA, "BANDANA LEVEL");
+	if( pos )
+	{
+		$levelcontent = substr( $DATA, $pos );
+	}
+
+	if( isset($levelname) && isset($creator) && isset($levelcontent))
+	{
+		echo "Received level '$levelname' by '$creator'.\n";
+	} else {
+		echo "Invalid data: missing level name, creator or content!\n";
+	}
+	$result = mkdir( "tmplevels/$creator", 0775, true );
+	$result = file_put_contents( "tmplevels/$creator/$levelname.dat", $levelcontent );
+	echo "$result";
+?>
--- webhooks/github.php
+++ webhooks/github.php
@@ -1,9 +1,15 @@
 
 <html>
- <head>
-  <title>Github update script</title>
- </head>
- <body>
- <?php echo shell_exec("git pull"); ?> 
- </body>
+<head>
+	<title>Github update script</title>
+</head>
+<body>
+Updated
+	<?php
+		//chdir(dirname(__FILE__));
+		$result = shell_exec("git pull");
+		//$result = shell_exec("whoami");
+		echo "<pre>$result</pre>" ;
+	?>
+</body>
 </html>
